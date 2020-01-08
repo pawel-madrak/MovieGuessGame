599efc1fd9e98066f59e9fd704437c984b9ebdd5
Pawel Madrak <madrak6@gmail.com>
2020-01-08 20:42:31 +0100

    Random movie is displaying

--- Game.java
+++ Game.java
@@ -1,2 +1,40 @@
+import java.util.Random;
+import java.io.File;
+import java.util.Scanner;
+
+
 public class Game {
+
+public static void main(String [] args) throws Exception{
+
+    File file = new File("filmy.txt");
+    Scanner scanner = new Scanner(file);
+    Scanner sc = new Scanner(file);
+
+int film = 0;
+int count = 0;
+    Random random = new Random();
+
+    while(scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        film++;
+    }
+        int r = random.nextInt(film);
+
+    while(sc.hasNextLine()) {
+        String lines = sc.nextLine();
+        count++;
+        if (r == count) {
+            System.out.print(lines);
+        }
+
+    }
+
+
+
+
+
+}
+
+
 }
