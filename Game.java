40c685c614ed1403937eace878eb014db391efef
Pawel Madrak <madrak6@gmail.com>
2020-01-10 13:22:39 +0100

    Game completed

--- Game.java
+++ Game.java
@@ -1,40 +1,103 @@
-import java.util.Random;
 import java.io.File;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Game {
 
-public static void main(String [] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-    File file = new File("filmy.txt");
-    Scanner scanner = new Scanner(file);
-    Scanner sc = new Scanner(file);
+        File file = new File("filmy.txt");
+        Scanner scanner = new Scanner(file);
+        Scanner sc = new Scanner(file);
 
-int film = 0;
-int count = 0;
-    Random random = new Random();
+        int film = 0;
+        int count = 0;
+        int loseCounter = 0;
+        String movie = null;
 
-    while(scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        film++;
-    }
+        Random random = new Random();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            film++;
+        }
         int r = random.nextInt(film);
+        int wordLength;
 
-    while(sc.hasNextLine()) {
-        String lines = sc.nextLine();
-        count++;
-        if (r == count) {
-            System.out.print(lines);
+        while (sc.hasNextLine()) {
+            String lines = sc.nextLine();
+            count++;
+            if (r == count) {
+                movie = lines;
+            }
         }
 
+        // do tego miejsca losuje film
+
+        char[] movieChars = movie.toCharArray();
+        char[] movieChars2 = movie.toCharArray();
+        wordLength = movieChars.length;
+        for (int i = 0; i < wordLength; i++) {
+            if (movieChars[i] == ' ') {
+                ;
+            } else {
+                movieChars[i] = '_';
+            }
+        }
+
+
+        System.out.println(movieChars);
+        System.out.println("You have guessed 0 wrong letters, insert latter ");
+
+        //do tad losuje i zamienia tytul filmu
+
+        Scanner input = new Scanner(System.in);
+        String s = movie;
+
+        while (loseCounter < 10) {
+            char inputChar = input.next().charAt(0);
+            boolean letterFound=false;
+
+            for (int j = 0; j < movie.length(); j++) {
+                char c = s.charAt(j);
+
+
+                if (inputChar == c) {
+                    letterFound=true;
+                    movieChars[j] = inputChar;
+                }
+
+
+                }
+            if (letterFound==false){
+                loseCounter++;
+            }
+
+
+            System.out.print(movieChars);
+            System.out.println("   You have guessed " + loseCounter + " wrong letters ");
+            String win = String.valueOf(movieChars);
+
+            if (win.equals(s)) {
+                System.out.println(" you are a winner");
+            } else if (loseCounter==10) {
+                System.out.println("You loose, title: " +movie);
+            }
+
+            }
+
+
+        }
+
+
+
+
+
+
+
     }
 
 
 
 
-
-}
-
-
-}
